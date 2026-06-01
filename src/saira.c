0021806bd775cefc9d6eb59f95b26a1e5e07c2da
SurlyDuck <santoslucas347@gmail.com>
2026-06-01 19:58:13 -0300

    LC-3 tutorial: opcodes

--- src/saira.c
+++ src/saira.c
@@ -1,5 +1,4 @@
 #include <stdint.h>
-#include <stdio.h>
 
 #define MEM_ADDRESSES_NUM (1<<16)
 
@@ -13,12 +12,39 @@ enum {
 	REG6,   
 	REG7,   
 	REG_COND,   
-	REG_PC,
+	REG_PC, /*program counter */
 	REG_COUNT   
+};
+
+enum {
+	OP_BR = 0, /* branch */
+	OP_ADD,    /* add  */
+	OP_LD,     /* load */
+	OP_ST,     /* store */
+	OP_JSR,    /* jump register */
+	OP_AND,    /* bitwise and */
+	OP_LDR,    /* load register */
+	OP_STR,    /* store register */
+	OP_RTI,    /* unused */
+	OP_NOT,    /* bitwise not */
+	OP_LDI,    /* load indirect */
+	OP_STI,    /* store indirect */
+	OP_JMP,    /* jump */
+	OP_RES,    /* reserved (unused) */
+	OP_LEA,    /* load effective address */
+	OP_TRAP    /* execute trap */
+};
+
+enum {
+	FL_POS = 1 << 0, /* FLAG POSITIVE */
+	FL_ZRO = 1 << 1, /* FLAG ZERO     */
+	FL_NEG = 1 << 2  /* FLAG NEGATIVE */
+
 };
 
 
 uint16_t memory[MEM_ADDRESSES_NUM];
+uint16_t reg[REG_COUNT];
 
 int main(int argc, char **argv){
 	
