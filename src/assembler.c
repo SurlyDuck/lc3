7746b548d4062ffc661445ba7f7d74427ecec530
SurlyDuck <santoslucas347@gmail.com>
2026-06-04 23:08:01 -0300

    Assembler file reading

--- src/assembler.c
+++ src/assembler.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int OpenFile(char *filePath);
 
 int main(int argc, char **argv){
 	
@@ -7,7 +10,33 @@ int main(int argc, char **argv){
 		return 1;
 	}
 	
+	if(!OpenFile(argv[1])){
+		fprintf(stderr, "\nError: Couldn't open file.\n");	
+	}
 	
 
 	return 0;
 }
+
+void ReadFile(FILE *file){
+	fseek(file,0,SEEK_END);
+	long size = ftell(file);
+	fseek(file,0,SEEK_SET);
+	char p[size];
+	fread(p, 1, size, file);
+	
+}
+
+int OpenFile(char *filePath){
+	FILE *file = NULL;
+	if(!(file = fopen(filePath, "r"))){
+		return 0;
+	}
+	
+	ReadFile(file);
+
+	fclose(file);
+
+	return 1;
+}
+
