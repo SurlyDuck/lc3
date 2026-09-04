3da5301c1c54faa5009982093a04daa972958fdd
SurlyDuck <santoslucas347@gmail.com>
2026-09-03 22:47:47 -0300

    layout for snapshot testing

---
+++ src/tests/record.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+echo "TODO: assemble execute a list of programs in ../examples and record their output to expected.golden"
---
+++ src/tests/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+echo "TODO: execute a list of programs in ../examples, save it to output.golden and compare it to expected.golden"
